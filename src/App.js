e1009b4c8a8c1d7cc7a42b5aba8a2acad4de96d7
jugoncalves13 <julianieugeniosilva@gmail.com>
2023-09-12 16:46:30 -0300

    Listagem pronta

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import { Avatar, Button } from "@mui/material";
+import { Avatar, Button, Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useActionData } from "react-router-dom";
+import Filme from "./components/Filme";
 
 function App() {
+   
+   const [ filmes, setFilmes ] = useState();
+   const [ erro, setErro ] = useState();
+
+   useEffect( () => {
+      fetch( process.env.REACT_APP_BACKEND + "filmes", {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      } )
+      .then( (resposta) => resposta.json() )
+      .then( (json ) => { setFilmes( json ) } )
+      .catch( (erro)  => { setErro( true ) } )
+
+   },  [])
      
  return(
     <>
-    <h1>Home</h1>
-    <Button variant="contained">Contained</Button>
-    <Button variant="outlined">Outlined</Button>
-    <Avatar alt="Juliani" src="/static/images/avatar/2.jpg" />
+    <h1>Filmes</h1>
+    <Container sx={{
+         display:"flex",
+         flexFlow:"row",
+         flexWrap:"wrap",
+         gap:"2rem"
+    }}/>
+    { filmes && (
+      filmes. map( (filme, index ) => ( 
+         <Filme 
+                imagem={filme.imagem}
+                titulo={filme.titulo}
+                descricao={filme.descricao}
+                categoria={filme.categoria}
+                ano={filme.ano}
+                duracao={filme.duracao}
+         />
+      ))
+    )}
     </>
- )
+ );
 
-  
 }
 
 export default App;
